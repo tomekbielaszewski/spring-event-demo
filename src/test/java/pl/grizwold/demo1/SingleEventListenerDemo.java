d91121e6b7e55942781789657c9f189faa064552
tbielaszewski <tbielaszewski@objectivity.co.uk>
2018-07-20 17:08:27 +0200

    demo for single event and single listener

---
+++ src/test/java/pl/grizwold/demo1/SingleEventListenerDemo.java
@@ -0,0 +1,48 @@
+package pl.grizwold.demo1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.grizwold.listeners.VoidReturningEventListener;
+import pl.grizwold.model.Event;
+
+import java.util.function.Consumer;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = SingleEventListenerDemo.Config.class)
+public class SingleEventListenerDemo {
+
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
+
+    @Autowired
+    private VoidReturningEventListener eventListener;
+
+    @Mock
+    private Consumer<Event> mockConsumer;
+
+    @Test
+    public void should_execute_listener_when_raising_single_event() throws Exception {
+        eventListener.consumer = mockConsumer;
+        Event event = new Event();
+        event.name = "single event";
+        eventPublisher.publishEvent(event);
+
+        Mockito.verify(mockConsumer).accept(event);
+    }
+
+    @Configuration
+    static class Config {
+        @Bean
+        public VoidReturningEventListener voidReturningEventListener() {
+            return new VoidReturningEventListener();
+        }
+    }
+}
